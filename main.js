e656cef2888e805bdc3def2da30e60ef986d3d8a
Filip Thuresson <60510154+FilipThureson@users.noreply.github.com>
2022-01-18 00:00:28 +0100

    see orbit lines

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ import { Vector3 } from 'three';
 
 
 const scene = new THREE.Scene();
-const G = 0.01;
+let G = 1;
+let numOfPlanets = 10;
 var dir = new THREE.Vector3();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100000);
@@ -20,33 +21,28 @@ const sunTexture = new THREE.TextureLoader().load('Solarsystemscope_texture_2k_s
 const earthTexture = new THREE.TextureLoader().load('earth.jpg');
 
 
+
+
 renderer.setPixelRatio(window.devicePixelRatio);
 
 renderer.setSize(window.innerWidth, window.innerHeight)
 camera.position.setY(500);
 camera.rotateY(Math.PI);
 const sun = new Body(1000, sunTexture, {x: 0, y: 0, z: 0}, {x:0, z:0, y:0}, 25,);
-const planet = new Body(50, earthTexture, {x: 0, y: 0, z: 0}, {x: 0, y: 0, z: 300},5);
-const planet2 = new Body(50, earthTexture, {x: 0, y: 0, z: 0}, {x: 0, y: 0, z: 100},5);
-const planet3 = new Body(50, earthTexture, {x: 0, y: 0, z: 0}, {x: 0, y: 0, z: 500},5);
-const planet4 = new Body(50, earthTexture, {x: 0, y: 0, z: 0}, {x: 0, y: 0, z: 800},5);
-
 
 //const gridHelper = new THREE.GridHelper(1000,500)
 //scene.add(gridHelper)
-
+var planets = [];
 scene.add(sun.body);
-scene.add(planet.body);
-scene.add(planet2.body);
-scene.add(planet3.body);
-scene.add(planet4.body);
 
+for(let i = 0; i < numOfPlanets; i++){
+  planets.push( new Body((Math.random()*40)+10, earthTexture, {x: 0, y: 0, z: 0}, {x: 0, y: 0, z: (Math.random()*1000) + 100}, (Math.random()*10)+10) );
+}
 
+for(let i = 0; i < planets.length; i++){
+  scene.add(planets[i].body);
+}
 
-
-const arrowHelper = new THREE.ArrowHelper(sun.body.position, planet.body.position , 100, 0xffff00);
-console.log(arrowHelper);
-//scene.add(arrowHelper);
 
 const pointLight = new THREE.PointLight(0xffffff);
 const ambientLight = new THREE.AmbientLight(0xffffff);
@@ -64,19 +60,14 @@ function animate(){
   requestAnimationFrame( animate );
 
   sun.body.rotation.y += G;
-  planet.body.rotation.y += G*2;
-  //planet.body.position.x += 0.05;
 
-  planet.update();
-  planet2.update();
-  planet3.update();
-  planet4.update();
 
-  sun.attract(planet);
-  sun.attract(planet2);
-  sun.attract(planet3);
-  sun.attract(planet4);
+  for(let i = 0; i < planets.length; i++){
 
+    planets[i].body.rotation.y += G*2;
+    planets[i].update();
+    sun.attract(planets[i]);
+  }
 
   renderer.render(scene, camera);
 }
@@ -94,7 +85,11 @@ function Body(_mass, _color, _vel, _pos, _r){
     this.vel.x = Math.sqrt((G*1000)/_pos.z);
   }
 
-
+  let orbitG = new THREE.TorusGeometry(_pos.z, 1, 16, 650)
+  let orbitM = new THREE.MeshStandardMaterial({color: 0xffffff});
+  let orbit = new THREE.Mesh( orbitG, orbitM);
+  scene.add(orbit);
+  orbit.rotateX(300);
 
   let geometry = new THREE.SphereGeometry(_r);
   let material = new THREE.MeshStandardMaterial({map: _color});
@@ -136,17 +131,11 @@ function Body(_mass, _color, _vel, _pos, _r){
 
     
 
-    arrowHelper.setDirection(dir);
-    arrowHelper.position.set(childV.x, childV.y, childV.z);
-    arrowHelper.setLength(dir.length() * 10000 );
+
 
     let vinkel = Math.atan2(child.body.position.z , child.body.position.x);
-
-    console.log(vinkel * (180/Math.PI));
     
     let f = new Vector3(dir.length()*Math.cos(vinkel), 0, (dir.length()*Math.sin(vinkel)));
-    console.log(f);
-    
     child.test(f);
   }
 
